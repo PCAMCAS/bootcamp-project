02ee4c251b365e433349aa2c8c54614baa574b06
Pedro Campos <castiwb@gmail.com>
2026-03-12 19:10:09 +0100

    style: migrate interface to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./**/*.{html,js}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
